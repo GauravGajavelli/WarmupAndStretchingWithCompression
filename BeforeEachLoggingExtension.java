5d54d9bfdac731118b86aa9e92677335b2a60288
gajavegs <gauravsg2004@gmail.com>
2025-06-30 02:01:41 -0500

    Adding runTime logging

--- BeforeEachLoggingExtension.java
+++ BeforeEachLoggingExtension.java
@@ -19,6 +19,6 @@ public class BeforeEachLoggingExtension implements BeforeEachCallback {
 
         // Optionally get the file name (assuming standard naming and location)
         
-        LoggingSingleton.addRunNumberToTest(testFileName, testName);;
+        LoggingSingleton.addRunNumberToTest(testFileName, testName);
 	}
 }
