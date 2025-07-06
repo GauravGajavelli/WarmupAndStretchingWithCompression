169ecfc1d6a3409211ea79eae9fd4a43f361da8e
gajavegs <gauravsg2004@gmail.com>
2025-07-06 07:47:59 -0500

    Completed refactor to use Store, working atomic saves

--- LoggingSingleton.java
+++ LoggingSingleton.java
@@ -12,7 +12,6 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class LoggingSingleton {
 	static private int jUnitTestCounter; // Keeps count of the number of JUnit tests
-	static private StringBuilder sB1;
 	static private int failureCount;
 	static private String timestamp;
 	static private boolean operationSupported;
@@ -30,7 +29,6 @@ public class LoggingSingleton {
 
     private LoggingSingleton() {
     	LoggingSingleton.jUnitTestCounter = 0;
-    	LoggingSingleton.sB1 = new StringBuilder();
     	LoggingSingleton.failureCount = 0;
     	LoggingSingleton.timestamp = new Timestamp(System.currentTimeMillis()).toString();
     	LoggingSingleton.operationSupported = true;
@@ -44,43 +42,24 @@ public class LoggingSingleton {
 		}
     }
     
-    public static LoggingSingleton getInstance(String testClassName) {
+    public static LoggingSingleton getInstance() {
         if (instance == null) {
             instance = new LoggingSingleton();
         }
-        LoggingSingleton.sB1.append(testClassName + ",");
         return instance;
     }
     
     public static void addTestPass() {
     	LoggingSingleton.jUnitTestCounter++;
-    	LoggingSingleton.sB1.append(LoggingSingleton.jUnitTestCounter + "T ");    	
     }
     
     public static void addTestFail() {
     	LoggingSingleton.jUnitTestCounter++;
     	LoggingSingleton.failureCount++;
-    	LoggingSingleton.sB1.append(LoggingSingleton.jUnitTestCounter + "F ");    	
     }
     
     public static void addTimeStamp() {
-    	int messageLength = LoggingSingleton.sB1.length();
-    	if (messageLength > 0) {
-    		// The last test pass or test fail appended added an additional " " at the end
-    		// Change that space to a ","
-    		int lastCharLocation = messageLength - 1;
-    		if (LoggingSingleton.sB1.charAt(lastCharLocation) == ' ') {
-    			LoggingSingleton.sB1.setCharAt(lastCharLocation, ',');
-    		}
-    	}
-    	if (LoggingSingleton.failureCount > 0) {
-    		// Operation under test failed one or more JUnit tests
-    		LoggingSingleton.sB1.append("0,");
-    	} else {
-    		// Operation under test passed all JUnit tests
-    		LoggingSingleton.sB1.append("1,");
-    	}
-    	LoggingSingleton.sB1.append(LoggingSingleton.timestamp);
+
     }
     
     public static void recordUnsupportedOperation() {
@@ -90,10 +69,6 @@ public class LoggingSingleton {
     
     public static boolean isOperationSupported() {
     	return LoggingSingleton.operationSupported;
-    }
-    
-    public static String getFullMessage() {
-    	return LoggingSingleton.sB1.toString();
     }
     
     public static ObjectMapper getObjectMapper() {
